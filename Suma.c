714e5d17ee5d1fa6f1daaf1104cfe62cfb0d53af
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-12 22:18:33 -0500

    segundo commit

--- Suma.c
+++ Suma.c
@@ -4,11 +4,19 @@ int main(void){
 
 int num;
 int res;
-
-printf("Coloque un numero entero positivo\n");
-scanf("%d", &num);
-
-res=num*(num+1)/2;
-
-printf("El resultado de la suma es: %d\n", res);
+while (num<=0)
+{
+    printf("El numero tiene que ser un entero positivo\n");
+    printf("Coloque un numero \n");
+   scanf("%d", &num);
+   if (num>0){
+    res=num*(num+1)/2;
+    printf("El resultado de la suma es: %d\n", res);
+   }
 }
+}
+
+
+
+
+
